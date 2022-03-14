aabaaf5be3b22a96714bfee2215a3ea177d8f14a
Timothy Butler <tbutler1132@gmail.com>
2022-03-14 12:31:13 -0400

    Added delete and refactored edit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,51 +1,172 @@
 import { useState, useEffect } from "react";
-import { lineItemsDB } from "../database";
+import { materialItemsDB, laborItemsDB, inclusiveItemsDB } from "../database";
+import ReactModal from 'react-modal'
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+ReactModal.setAppElement('#root');
 
 function Project() {
     const [lineItems, setLineItems] = useState([])
 
+    const [materialItems, setMaterialItems] = useState([])
+    const [laborItems, setLaborItems] = useState([])
+    const [inclusiveItems, setInclusiveItems] = useState([])
+    // const [laborItems, setLaborItems] = useState([])
+    // const [inclusiveItems, setInclusiveItems] = useState([])
+
     useEffect(() => {
-        setLineItems(lineItemsDB)
-    })
+
+        // console.log("Hit ?")
+        // const materialArr = []
+        // const laborArr = []
+        // const inclusiveArr = []
+
+        // lineItemsDB.forEach(item => {
+        //     if(item.type === "material"){
+        //         materialArr.push(item)
+        //     }
+        //     if(item.type === "labor"){
+        //         laborArr.push(item)
+        //     }
+        //     if(item.type === "inclusive"){
+        //         inclusiveArr.push(item)
+        //     }
+        // })
+
+        // setLineItems(lineItemsDB)
+
+        setMaterialItems(materialItemsDB)
+        setLaborItems(laborItemsDB)
+        setInclusiveItems(inclusiveItemsDB)
+        // setLaborItems(laborArr)
+        // setInclusiveItems(inclusiveArr)
+    }, [])
 
 
-    const updateItem = (id, {cost, title}) => {
-        console.log(id, cost, title)
-        const idx = lineItems.findIndex(item => item.id === id)
-        setLineItems(lineItems.splice(idx, 1, {id, cost, title}))
+    const updateItems = (id, type, {cost, title}) => {
+        
+        const updatedArray = (array) => {
+            let idx = array.findIndex(item => item.id === id)
+            let arr = [...array]
+            arr.splice(idx, 1, { id, title, cost })
+            return arr
+        }
+
+        if(type === "Material"){
+            setMaterialItems(updatedArray(materialItems))
+        }
+        if(type === "Labor"){
+            setLaborItems(updatedArray(laborItems))
+        }
+        if(type === "Inclusive"){
+            setInclusiveItems(updatedArray(inclusiveItems))
+        }
     }
 
-    const deleteItem = (id) => {
+
+    const deleteItem = (id, type) => {
+        const updatedArray = (array) => {
+            let idx = array.findIndex(item => item.id === id)
+            let arr = [...array]
+            arr.splice(idx, 1)
+            return arr
+        }
+
+        if(type === "Material"){
+            setMaterialItems(updatedArray(materialItems))
+        }
+        if(type === "Labor"){
+            setLaborItems(updatedArray(laborItems))
+        }
+        if(type === "Inclusive"){
+            setInclusiveItems(updatedArray(inclusiveItems))
+        }
 
     }
 
-    const calculateEstimate = () => {
+    const calculateEstimate = (arr) => {
         let estimate = 0
-        lineItems.forEach(item => {
+        arr.forEach(item => {
             estimate = estimate + Number(item.cost)
         })
         return estimate
     }
 
+    const sortItems = () => {
+        const obj = {
+            materialItems: [],
+            laborItems: [],
+            inclusiveItems: [],
+        }
+        
+        lineItems.forEach(item => {
+            if(item.type === "material"){
+                obj.materialItems.push(item)
+            }
+            if(item.type === "labor"){
+                obj.laborItems.push(item)
+            }
+            if(item.type === "inclusive"){
+                obj.inclusiveItems.push(item)
+            }
+        })
 
-    const renderLineItems = () => {
-        return lineItems.map(item => 
-            <LineItem key={item.id} item={item} update={updateItem}/>
-        )
+        return obj
     }
+
+
+    // const renderLineItems = () => {
+    //     return lineItems.map(item => 
+    //         <LineItem key={item.id} item={item} update={updateItem}/>
+    //     )
+    // }
 
     return (
         <div>
-            <h1>Estimate: {calculateEstimate()}</h1>
-            <h3>Material Costs</h3>
-            <ul>{renderLineItems()}</ul>
-            <h3>Labor Costs</h3>
-            <h3>All Inclusive Costs</h3>
+            <h1>Estimate: ${calculateEstimate(lineItems)}</h1>
+            <div className="main-container">
+                <LineItemsContainer type="Material" calculateEstimate={calculateEstimate} items={materialItems} updateItem={updateItems} deleteItem={deleteItem} />
+                <LineItemsContainer type="Labor" calculateEstimate={calculateEstimate} items={laborItems} updateItem={updateItems} deleteItem={deleteItem}/>
+                <LineItemsContainer type="Inclusive" calculateEstimate={calculateEstimate} items={inclusiveItems} updateItem={updateItems} deleteItem={deleteItem}/>
+            </div>
         </div>
     );
 }
 
-function LineItem({ item, update }){
+function LineItemsContainer({ type, items, updateItem, calculateEstimate, deleteItem }){
+    
+
+    const renderLineItems = () => {
+        return items.map(item => 
+            <LineItem type={type} key={item.id} item={item} update={updateItem} deleteItem={deleteItem}/>
+        )
+    }
+
+    const addItemHandler = () => {
+
+    }
+
+    return (
+        <div className="container-child">
+            <h3>{type} Costs</h3>
+            <button>Add {type} Cost</button>
+            <p>Total: {calculateEstimate(items)}</p>
+            <ul>{renderLineItems()}</ul>    
+        </div>
+    )
+}
+
+function LineItem({ item, type, update, deleteItem }){
     const [title, setTitle] = useState("")
     const [cost, setCost] = useState(0)
     const [editMode, toggleEditMode] = useState(false)
@@ -66,37 +187,66 @@ function LineItem({ item, update }){
 
     const submitHandler = (e) => {
         toggleEditMode(false)
-        update(item.id, {cost, title})
+        update(item.id, type, {cost, title})
+    }
+
+    const deleteHandler = (e) => {
+        deleteItem(item.id, type)
+    }
+
+    function openModal() {
+        toggleEditMode(true);
+    }
+
+    
+    function closeModal() {
+        toggleEditMode(false);
+    }
+
+
+    const renderModal = () => {
+       return (
+        <ReactModal
+            isOpen={editMode}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+            <div>
+                <form onSubmit={submitHandler}>
+                    <input
+                    defaultValue={title}
+                    onChange={titleChangeHandler} 
+                    />
+                    <input
+                    defaultValue={cost}
+                    onChange={costChangeHandler}  
+                    type="number"
+                    />
+                    <button type="submit">Save</button>
+                </form>
+                <button onClick={closeModal}>close</button>
+            </div>
+        </ReactModal>   
+       ) 
     }
 
     return (
         <div>
         {editMode 
         ? 
-            <li>
-                <div style={{border: "solid"}}>
-                    <form onSubmit={submitHandler}>
-                        <input
-                        defaultValue={title}
-                        onChange={titleChangeHandler} 
-                        />
-                        <input
-                        defaultValue={cost}
-                        onChange={costChangeHandler}  
-                        type="number"
-                        />
-                        <button type="submit">Save</button>
-                    </form>
-                    <button>Delete</button>   
-                </div>
-            </li>
+            renderModal()
         :
             <li>
-                <div style={{border: "solid"}}>
-                    <span style={{color: "black"}}>{item.title}</span>
-                    <span>{item.cost}</span>
-                    <button onClick={clickHandler}>Edit</button>
-                    <button>Delete</button>   
+                <div className="line-item">
+                    <div style={{display: "flex", justifyContent: "space-around", width: "50%" }}>
+                        <span style={{color: "black"}}>{item.title}</span>
+                        <span>${item.cost}</span>
+                    </div>
+                    <div>
+                        <button onClick={clickHandler}>Edit</button>
+                        <button onClick={deleteHandler}>Delete</button>   
+                    </div>
                 </div>
             </li>
         }
--- src/database.js
+++ src/database.js
@@ -1,12 +1,53 @@
-export const lineItemsDB = [
+export const materialItemsDB = [
     {
         id: 1,
         title: "Roof",
-        cost: 10
+        cost: 10,
     },
     {
         id: 2,
         title: "Roof",
-        cost: 15
+        cost: 15,
+    },
+    {
+        id: 3,
+        title: "Dude",
+        cost: 20,
+    },
+]
+
+export const laborItemsDB = [
+    {
+        id: 1,
+        title: "L1",
+        cost: 10,
+    },
+    {
+        id: 2,
+        title: "L2",
+        cost: 15,
+    },
+    {
+        id: 3,
+        title: "L3",
+        cost: 20,
+    },
+]
+
+export const inclusiveItemsDB = [
+    {
+        id: 1,
+        title: "Q1",
+        cost: 10,
+    },
+    {
+        id: 2,
+        title: "Q2",
+        cost: 15,
+    },
+    {
+        id: 3,
+        title: "Q3",
+        cost: 20,
     },
 ]
